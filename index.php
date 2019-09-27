5c80f76f2d7d234c97dd87d42bed03bcf0a27eeb
teshavat <42034738+teshavat@users.noreply.github.com>
2019-09-27 14:58:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot; 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
-$channel_token = '1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/w1cDnyilFU='; 
-$channel_secret = '9b2c7349ea939ef723a3cb453d774c86';
+$channel_token = 'pyXzvm4/zvH7FT3DbwNuFIQd6Ql8y9zL9aHb/9otnLEUDoHupFrNx1MqZprwKYevmeUX+GnCBRWfu/u20qgaStD0dZ3WsOBZsyBJnqKpsvoXxjog6vq+fUaAmHUsOKp7ne6JFs9bZrX2IJ6AZg0nfAdB04t89/1O/w1cDnyilFU='; 
+$channel_secret = '843086e01502017642384904eaf042c5';
 
 // Get message from Line API 
 $content = file_get_contents('php://input'); 
