928d900793f7429d127f8db16f6108a3656a8f4f
teshavat <42034738+teshavat@users.noreply.github.com>
2019-10-01 10:11:18 +0700

    Update testNotif.php

--- testNotif.php
+++ testNotif.php
@@ -1,19 +1,21 @@
 <?php 
 define('LINE_API', 'https://notify-api.line.me/api/notify'); 
 
-$message = array( 
-  'message' => 'Hello. สวัสดีครับ', 
-); 
+//$message = array( 'message' => 'Hi', 'imageThumbnail' => 'https://raw.githubusercontent.com/platoosom/line-notify-example/master/unsplash-thumbnail.jpg', 'imageFullsize' => 'https://raw.githubusercontent.com/platoosom/line-notify-example/master/unsplash-fullsize.jpg', ); $
+$message = array( 'message' => 'Hi', 'stickerPackageId' => 1, 'stickerId' => 5, );
+//$message = array(   'message' => 'Hello. <a href="simpelgroup.com>SImpel</a>', ); 
 
 $tokens = file_get_contents('tokens.json'); 
 $tokens = json_decode($tokens, true); 
 
-echo("----");
+echo "The time is " . date("h:i:sa");
+
+//echo("----");
 foreach($tokens as $token){ 
   line_notify($token, $message); 
-  echo($token);
+  //echo($token);
 } 
-echo("----");
+//echo("----");
 
 function line_notify($token, $message) { 
   $header = array( 
